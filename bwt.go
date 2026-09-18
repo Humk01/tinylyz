c0186b4839769c0c5622a8bbae26c687b6d73202
Humk <bitrusseth06@gmail.com>
2026-09-18 19:01:01 +0100

    feature: implement the reverse bwt transfer using lf tables

--- bwt.go
+++ bwt.go
@@ -5,6 +5,7 @@ import (
 	"slices"
 )
 
+// creates the bwt
 func Forward(word []byte) ([]byte, int) {
 	primaryIndex := 0
 	bwt_slice := [][]byte{}
@@ -27,4 +28,33 @@ func Forward(word []byte) ([]byte, int) {
 		bwt = append(bwt, v[length-1])
 	}
 	return bwt, primaryIndex
+}
+
+// reverses the bwt transform
+func Inverse(word []byte, num int) []byte {
+    index := num
+    length := len(word)
+    sorted := slices.Clone(word)
+    slices.Sort(sorted)
+    final := make([]byte, length)
+    start := make(map[byte]int)
+    seen := make(map[byte]int)
+
+    // build start once, from F
+    for i := range length {
+        c := sorted[i]
+        if _, ok := start[c]; !ok {
+            start[c] = i
+        }
+    }
+
+    // walk, filling final from the back
+    for i := range length {
+        current := word[index]
+        final[length-1-i] = current
+        seen[current] += 1
+        index = start[current] + seen[current] - 1
+    }
+
+    return final
 }
